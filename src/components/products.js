2ec13403c356922cc54e1f89ad80c2324550b384
victorandres20 <vapedraza1706@gmail.com>
2024-07-01 17:24:46 -0500

    fix product boxed width

--- src/components/products.js
+++ src/components/products.js
@@ -17,9 +17,9 @@ export function Products({ groups, addFunction = () => {} }){
             <Box sx={{ width: '100%', height: '500px', display: 'flex', flexDirection: 'column', overflowY: 'auto' }}>
                 {
                     groups.map((group, index) => (
-                        <Box key={index} sx={{ padding: '20px 20px' }}>
-                            <Card onClick={() => setGroupSelected(group['group'])}>
-                                <Box sx={{ width: '250px', height: '80px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+                        <Box key={index} sx={{ width: '150px', padding: '20px 20px' }}>
+                            <Card style={{ cursor: 'pointer' }} onClick={() => setGroupSelected(group['group'])}>
+                                <Box sx={{ width: '100%', height: '80px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
                                     {group['group']}
                                 </Box>
                             </Card>
@@ -32,9 +32,9 @@ export function Products({ groups, addFunction = () => {} }){
             <Box sx={{ width: '100%', height: '500px', display: 'flex', flexDirection: 'column', overflowY: 'auto' }}>
                 {
                     products.map((product, index) => (
-                        <Box key={index} sx={{ padding: '20px 20px' }}>
-                            <Card onClick={() => addFunction(product)}>
-                                <Box sx={{ width: '250px', height: '80px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+                        <Box key={index} sx={{  width: '150px', padding: '20px 20px' }}>
+                            <Card style={{ cursor: 'pointer' }} onClick={() => addFunction(product)}>
+                                <Box sx={{ width: '100%', height: '80px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
                                     {product['descrip']}
                                 </Box>
                             </Card>
